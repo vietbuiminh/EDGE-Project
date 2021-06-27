02c953e553a81141a4ced74a2406ddcb6af9de20
Viet Bui <buiminhviet.2002@gmail.com>
2021-06-27 16:32:22 -0500

    Added the responsive burger nav

--- CoolQC/script.js
+++ CoolQC/script.js
@@ -3,6 +3,29 @@ var people = document.getElementById("people-trigger");
 var places = document.getElementById("places-trigger");
 var progress = document.getElementById("progress-trigger");
 var discoverinfo = document.getElementById("discover-info");
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".mobile-sticky-nav");
+    const navLinks = document.querySelectorAll(".mobile-sticky-nav-menu a");
+    // Toggle Nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        //Animation
+        navLinks.forEach((a, index) => {
+            if (a.style.animation) {
+                a.style.animation = '';
+            } else {
+                a.style.animation = `mobileNavFade 0.5s ease forwards ${index / 7 + 0.2}s`;
+            }
+        });
+
+        // burger animation
+        burger.classList.toggle('toggle');
+    });
+    
+}
+
 
 window.addEventListener('load', () => {
     const preload = document.querySelector(".preload");
@@ -11,13 +34,15 @@ window.addEventListener('load', () => {
 
 window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
-        if (prevScrollpos < currentScrollPos && ((document.body.scrollTop > 50 || document.documentElement.scrollTop > 50))) {
-        document.getElementById("sticky-nav").style.bottom = "0";
-        } else {
-        document.getElementById("sticky-nav").style.bottom = "-200px";
-        }
-        prevScrollpos = currentScrollPos;
+    if (prevScrollpos < currentScrollPos && ((document.body.scrollTop > 50 || document.documentElement.scrollTop > 50))) {
+    document.getElementById("sticky-nav").style.bottom = "0";
+    } else {
+    document.getElementById("sticky-nav").style.bottom = "-200px";
     }
+    prevScrollpos = currentScrollPos;
+}
+
+navSlide();
 
 people.addEventListener("mouseover", changePeople);
 people.addEventListener("mouseout", orignalTitle);
