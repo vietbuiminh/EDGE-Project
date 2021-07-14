fa69d14ee289a1546bbc55b6a9ff7245a5e8002d
Viet Bui <buiminhviet.2002@gmail.com>
2021-07-13 22:54:24 -0500

    responsive website

--- lulac/js/responsiveNavBar.js
+++ lulac/js/responsiveNavBar.js
@@ -6,3 +6,26 @@ navBarToggle.addEventListener('click', function () {
     burger.classList.toggle('toggle');
     mainNav.classList.toggle('active');
 });
+
+const header = document.querySelector("header");
+const navbar = header.querySelector(".navbar");
+const headerbg = document.querySelector(".bg");
+
+const headerbgOptions = {
+    rootMargin: "-200px 0px 0px 0px"
+};
+
+const headerbgObserver = new IntersectionObserver(function(
+    entries, 
+    headerbgOptions
+    ) {
+        entries.forEach(entry => {
+            if(!entry.isIntersecting) {
+                navbar.classList.add("scrolled");
+            } else if (entry.isIntersecting && navbar.classList.contains("scrolled")){
+                navbar.classList.remove("scrolled");
+            }
+        })
+    }, headerbgOptions);
+
+headerbgObserver.observe(headerbg);
