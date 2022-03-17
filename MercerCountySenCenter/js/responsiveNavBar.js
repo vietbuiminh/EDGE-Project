aeb684734525f63a9f5be8ae927481768e50a65b
Viet Bui <buiminhviet.2002@gmail.com>
2022-03-17 14:24:35 -0500

    Starting to document the way to make website fast in EDGE

---
+++ MercerCountySenCenter/js/responsiveNavBar.js
@@ -0,0 +1,10 @@
+// Code written by Viet (Ethan) Bui
+// This responsive nav bar is good for simple nav bar without the drop down nav
+const mainNav = document.getElementById('menu');
+const navBarToggle = document.getElementById('js-navbar-toggle');
+const burger = document.querySelector(".burger");
+
+navBarToggle.addEventListener('click', function () {
+    burger.classList.toggle('toggle');
+    mainNav.classList.toggle('active');
+});
