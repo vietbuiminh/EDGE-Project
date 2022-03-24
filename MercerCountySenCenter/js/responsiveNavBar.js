ace6ab1360705de292e0062d75623e7767dd959d
Viet Bui <buiminhviet.2002@gmail.com>
2022-03-24 12:48:22 -0500

    Building the page

--- MercerCountySenCenter/js/responsiveNavBar.js
+++ MercerCountySenCenter/js/responsiveNavBar.js
@@ -8,3 +8,39 @@ navBarToggle.addEventListener('click', function () {
     burger.classList.toggle('toggle');
     mainNav.classList.toggle('active');
 });
+
+// this part to turn the navigation bar to stick to the top after scoll pass the size of the screen
+const navbar = document.querySelector(".navbar");
+const height = window.innerHeight;
+const headerbg = document.querySelector(".headerwrapper");
+console.log(height);
+
+
+
+const headerbgOptions = {
+    rootMargin: "-" + (height/50) + "px 0px 0px 0px"
+};
+const headerbgObserver = new IntersectionObserver(function(
+    entries, 
+    headerbgOptions
+    ) {
+        entries.forEach(entry => {
+            if(!entry.isIntersecting) {
+                navbar.classList.add("scrolled");
+                navbar.classList.remove("hide");
+                navbar.classList.add("scrolled-animated");
+                
+            } else if (entry.isIntersecting && navbar.classList.contains("scrolled")){
+                navbar.classList.remove("scrolled");
+                navbar.classList.remove("scrolled-animated");
+                navbar.classList.add("hide");
+            }
+        })
+    }, headerbgOptions);
+//Only use this in Home Page
+if (document.getElementById('Home')) {
+    headerbgObserver.observe(headerbg);
+} else {
+    navbar.classList.add("scrolled");
+}
+
